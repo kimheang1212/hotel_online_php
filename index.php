fded84bad6547303d9094f11b627f9932b253168
kimheang1212 <kimheangheng1@gmail.com>
2023-08-14 19:00:06 +0700

    test font

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Heang Hotel</a>
+    <a class="navbar-brand me-5 fw-bold fs-3 h-font" href="index.php">Heang Hotel</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -31,10 +31,10 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Rooms</a>
+          <a class="nav-link" href="#">Rooms</a> <!-- បន្ទប់ -->
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Facilities</a>
+          <a class="nav-link" href="#">Facilities</a><!-- គ្រឿងបរិក្ខារ -->
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Contanct Us</a>
